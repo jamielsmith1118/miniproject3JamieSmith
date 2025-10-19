49b7febf98f676929ce39d9b556fa39f97df93df
Jamie Smith <jamielsmith1118@gmail.com>
2025-10-18 19:28:35 -0500

    Added auth blueprint. Began adding base.html

--- Project FortiDragon/__init__.py
+++ Project FortiDragon/__init__.py
@@ -38,4 +38,9 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    # Import auth blueprint
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+
     return app
